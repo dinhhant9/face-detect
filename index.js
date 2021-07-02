acf046032a1d71d44457fc48853b6d85f0dd4387
Dinh Han <tdhan@tma.com.vn>
2021-07-02 18:03:43 +0700

    Update index

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.post("/upload", async (req, res) => {
     });
 });
 
+app.get("/", async (req, res) => {
+    res.send("Han pro");
+});
+
 app.listen(port, () => {
     console.log("Server started on port:" + port);
 });
