1bbbccb73cd99665b77b859526298ad0d1401c3a
Dinh Han <tdhan@tma.com.vn>
2021-07-02 17:57:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const faceApiService = require("./faceapiService")
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(fileUpload());
+
+app.post("/upload", async (req, res) => {
+    const { file } = req.files;
+
+    const result = await faceApiService.detect(file.data);
+
+    res.json({
+        detectedFaces: result,
+    });
+});
+
+app.listen(port, () => {
+    console.log("Server started on port:" + port);
+});
